0c342a9a2072b92fc9fe406665a9db09f71873a9
kamil.hawdziejuk <kamil.hawdziejuk@f06b5e36-16d3-5707-c7e6-8ec0992c4e53>
2010-07-20 19:12:04 +0000

    - creating library XELibrary with components - creating three GameComponents: Camera, InputHangler, FPS nad using them in the XNAGAME:)

---
+++ trunk/XELibrary/Camera.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace XELibrary
+{
+    public class Camera : GameComponent
+    {
+        private IInputHandler input;
+        private GraphicsDeviceManager graphics;
+
+        public Vector3 cameraPosition = new Vector3(0.0f, 0.0f, 3.0f);
+        public Vector3 cameraTarget = Vector3.Zero;
+        public Vector3 cameraUpVector = Vector3.Up;
+        private Matrix projection;
+        private Matrix view;
+
+        public Camera(Game game) : base(game)
+        {
+            graphics = (GraphicsDeviceManager)Game.Services.GetService(typeof(IGraphicsDeviceManager));
+            input = (IInputHandler)Game.Services.GetService(typeof(IInputHandler));
+        }
+
+        public Matrix View
+        {
+            get { return this.view; }
+        }
+
+        public Matrix Projection
+        {
+            get { return this.projection; }
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            this.InitializeCamera();
+        }
+
+        private void InitializeCamera()
+        {
+            float aspectRatio = (float)graphics.GraphicsDevice.Viewport.Width / (float)graphics.GraphicsDevice.Viewport.Height;
+            Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 0.0001f, 1000.0f, out this.projection);
+            Matrix.CreateLookAt(ref this.cameraPosition, ref this.cameraTarget, ref this.cameraUpVector, out this.view);
+        }
+    }
+}
---
+++ trunk/XELibrary/FPS.cs
@@ -0,0 +1,69 @@
+﻿//Kamil Hawdziejuk
+//19-07-2010
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace XELibrary
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public sealed partial class FPS: DrawableGameComponent
+    {
+        private float fps;
+        private float updateInterval = 1.0f;
+        private float timeSinceLastUpdate = 0.0f;
+        private float framecount = 0;
+        public FPS(Game game) : this(game, false, false, game.TargetElapsedTime) { }
+        public FPS(Game game, bool synchWithVerticalRetrace,  bool isFixedTimeStep, TimeSpan targetElapsedTime): base(game)
+        {
+            GraphicsDeviceManager graphics = (GraphicsDeviceManager)Game.Services.GetService(typeof(IGraphicsDeviceManager));
+            graphics.SynchronizeWithVerticalRetrace = synchWithVerticalRetrace;
+            Game.IsFixedTimeStep = isFixedTimeStep;
+            Game.TargetElapsedTime = targetElapsedTime;
+        }
+        /// <summary>
+        /// Allows the game component to perform any initialization
+        /// it needs to before starting to run. This is where it can query for
+        /// any required services and load content.
+        /// </summary>
+        public sealed override void Initialize()
+        {
+            // TODO: Add your initialization code here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name=”gameTime”>Provides snapshot of timing values.</param>
+        public sealed override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+            base.Update(gameTime);
+        }
+
+        public sealed override void Draw(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedRealTime.TotalSeconds;
+            framecount++;
+            timeSinceLastUpdate += elapsed;
+            if (timeSinceLastUpdate > updateInterval)
+            {
+                fps = framecount / timeSinceLastUpdate;
+#if XBOX360
+                System.Diagnostics.Debug.WriteLine("FPS: : + fps.ToString());
+#else
+                Game.Window.Title = "FPS: " + fps.ToString();
+#endif
+                framecount = 0;
+                timeSinceLastUpdate -= updateInterval;
+            }
+            base.Draw(gameTime);
+        }
+    }
+}
