c9d813fe01aac4d1fa018932c46f94fe08cc0f2b
kamil.hawdziejuk <kamil.hawdziejuk@f06b5e36-16d3-5707-c7e6-8ec0992c4e53>
2010-07-20 19:54:22 +0000

    Updating InputHandler that response af the Left & Right Keyboard pressed.

--- trunk/XELibrary/Camera.cs
+++ trunk/XELibrary/Camera.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace XELibrary
 {
@@ -17,10 +18,16 @@ namespace XELibrary
         private Matrix projection;
         private Matrix view;
 
+        private float cameraYaw = 0.0f;
+        private const float spinRate = 2.0f;
+
+        private Vector3 cameraReferance = new Vector3(0.0f, 0.0f, -1.0f);
+
         public Camera(Game game) : base(game)
         {
             graphics = (GraphicsDeviceManager)Game.Services.GetService(typeof(IGraphicsDeviceManager));
             input = (IInputHandler)Game.Services.GetService(typeof(IInputHandler));
+            input = (IInputHandler)Game.Components[1];
         }
 
         public Matrix View
@@ -41,6 +48,41 @@ namespace XELibrary
 
         private void InitializeCamera()
         {
+            float aspectRatio = (float)graphics.GraphicsDevice.Viewport.Width / (float)graphics.GraphicsDevice.Viewport.Height;
+            Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 0.0001f, 1000.0f, out this.projection);
+            Matrix.CreateLookAt(ref this.cameraPosition, ref this.cameraTarget, ref this.cameraUpVector, out this.view);
+        }
+
+        public override void  Update(GameTime gameTime)
+        {
+            if (input.KeyboardState.IsKeyDown(Keys.Left))
+            {
+                cameraYaw += spinRate;
+            }
+            if (input.KeyboardState.IsKeyDown(Keys.Right))
+            {
+                cameraYaw -= spinRate;
+            }
+
+            if (cameraYaw > 360)
+            {
+                cameraYaw -= 360;
+            }
+            else if (cameraYaw < 0)
+            {
+                cameraYaw += 360;
+            }
+ 	        base.Update(gameTime);
+
+            Matrix rotationMatrix;
+            Matrix.CreateRotationY(MathHelper.ToRadians(cameraYaw), out rotationMatrix);
+            //create a vector poinint the direction the camera is facing
+            Vector3 transformedReference;
+            Vector3.Transform(ref cameraReferance, ref rotationMatrix, out transformedReference);
+
+            //Calculate the position the camera is looking at
+            Vector3.Add(ref cameraPosition, ref transformedReference, out cameraTarget);
+
             float aspectRatio = (float)graphics.GraphicsDevice.Viewport.Width / (float)graphics.GraphicsDevice.Viewport.Height;
             Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 0.0001f, 1000.0f, out this.projection);
             Matrix.CreateLookAt(ref this.cameraPosition, ref this.cameraTarget, ref this.cameraUpVector, out this.view);
--- trunk/XELibrary/IInputHandler.cs
+++ trunk/XELibrary/IInputHandler.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
 namespace XELibrary
 {
     public interface IInputHandler
     {
+        KeyboardState KeyboardState { get; }
     }
 }
