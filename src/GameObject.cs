423e3cff8f99778f03a1dc33d93c2e806d2d8eb5
kamil.hawdziejuk <kamil.hawdziejuk@f06b5e36-16d3-5707-c7e6-8ec0992c4e53>
2010-07-27 19:24:35 +0000

    Adding some models & implementing moving car effect:)

---
+++ src/GameObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace GameXna
+{
+    /// <summary>
+    /// Main class that represents game's object
+    /// </summary>
+    public class GameObject
+    {
+        private Texture2D texture;
+        public Model Model;
+        public Matrix World;
+        public Matrix Scale;
+        private string name = string.Empty;
+
+        #region --- Constructing & destroying objects ---
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="_model"></param>
+        /// <param name="_world"></param>
+        /// <param name="_scale"></param>
+        public GameObject(Model _model, Matrix _world, string _name)
+        {
+            this.Model = _model;
+            this.World = _world;
+            this.position = new Vector3(0, 0, 0);
+            this.name = _name;
+        }
+
+        #endregion
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        #region --- Position ---
+
+        private Vector3 position;
+
+        public Vector3 Position
+        {
+            get
+            {
+                return this.position;
+            }
+            set
+            {
+                Vector3 movement = value - this.position;
+                this.position = value;
+                this.World *= Matrix.CreateTranslation(movement);
+            }
+        }
+
+        #endregion
+    }
+}
