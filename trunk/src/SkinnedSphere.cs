697a918da3c464f748155564e9b52d8a302ac71a
kamil.hawdziejuk <kamil.hawdziejuk@f06b5e36-16d3-5707-c7e6-8ec0992c4e53>
2010-08-11 18:32:32 +0000

    Adding functionality of rotating the choop over Y axe of an helicopter:)

---
+++ trunk/src/SkinnedSphere.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameXna
+{
+    /// <summary>
+    /// Stores a bounding sphere for checking collision against a skinned
+    /// model, and records which bone this sphere is attached to.
+    /// </summary>
+    public class SkinnedSphere
+    {
+        public string BoneName;
+        public float Radius;
+
+        [ContentSerializer(Optional = true)]
+        public float Offset;
+    }
+}
