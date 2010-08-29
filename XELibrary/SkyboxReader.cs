7335cf8d635bb41d7307edb4d281c56dd95cdc2a
kamil.hawdziejuk <kamil.hawdziejuk@f06b5e36-16d3-5707-c7e6-8ec0992c4e53>
2010-08-29 19:46:00 +0000

    Some collision detectors algorithms.

---
+++ XELibrary/SkyboxReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace XELibrary
+{
+    public class SkyboxReader : ContentTypeReader<Skybox>
+    {
+        protected override Skybox Read(ContentReader input, Skybox existingInstance)
+        {
+            return new Skybox(input);
+        }
+    }
+
+    public class Skybox
+    {
+        private Model skyboxModel;
+        private Texture2D skyboxTexture;
+        internal Skybox(ContentReader input)
+        {
+            skyboxModel = input.ReadObject<Model>();
+            skyboxTexture = input.ReadObject<Texture2D>();
+        }
+        public void Draw(Matrix view, Matrix projection, Matrix world)
+        {
+
+            foreach (ModelMesh mesh in skyboxModel.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.Projection = projection;
+                    be.View = view;
+                    be.World = world;
+                    be.Texture = skyboxTexture;
+                    be.TextureEnabled = true;
+                }
+                mesh.Draw(SaveStateMode.SaveState);
+            }
+        }
+    }
+}
---
+++ src/GameObjectsManager.cs
@@ -0,0 +1,87 @@
+﻿//07-08-2010
+//Kamil Hawdziejuk
+
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace GameXna
+{
+    public class GameObjectsManager : GameComponent
+    {
+        #region --- Private fields ---
+
+        private SphereCollisionDetector sphereCollisionDetector = new SphereCollisionDetector();
+        private List<GameObject> gameObjects = new List<GameObject>();
+        private GameObject activeObject = null;
+
+        #endregion
+
+        #region --- Creator & destroyers ---
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="game"></param>
+        public GameObjectsManager(Game game) : base(game)
+        {
+        }
+
+        #endregion
+
+        #region --- Public properties --- 
+
+        public GameObject ActiveObject
+        {
+            get
+            {
+                return this.activeObject;
+            }
+            set
+            {
+                this.activeObject = value;
+            }
+        }
+
+        public List<GameObject> GameObjects
+        {
+            get
+            {
+                return this.gameObjects;
+            }
+        }
+
+        #endregion
+
+        #region --- Public methods ---
+
+        public GameObject Get(string _name)
+        {
+            return this.gameObjects.Find(a => a.Name.Equals(_name));
+        }
+        
+        public void Add(GameObject _obj)
+        {
+            if (!this.gameObjects.Contains(_obj))
+            {
+                this.gameObjects.Add(_obj);
+            }
+        }
+
+        #endregion
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (GameObject obj in this.gameObjects)
+            {
+                if (obj != this.activeObject)
+                {
+                    if (sphereCollisionDetector.DetectCollision(obj, this.activeObject, 0))
+                    {
+                        //  break;
+                    }
+                }
+            }
+            base.Update(gameTime);
+        }
+    }
+}
