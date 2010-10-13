7813f2137a13cbe678609576b5e9f2cd97bd224f
kamil.hawdziejuk <kamil.hawdziejuk@f06b5e36-16d3-5707-c7e6-8ec0992c4e53>
2010-10-13 20:21:44 +0000

    Adding new sounds and collision detection mechanism

--- trunk/src/Collisions/SphereCollisionDetector.cs
+++ trunk/src/Collisions/SphereCollisionDetector.cs
@@ -13,17 +13,22 @@ namespace GameXna
 
         public bool DetectCollision(GameObject obj1, GameObject obj2, double tolerance)
         {
+            if (obj1.Model == null || obj2.Model == null)
+            {
+                return false;
+            }
+
             foreach (ModelMesh mesh1 in obj1.Model.Meshes)
             {
                 foreach (ModelMesh mesh2 in obj2.Model.Meshes)
                 {
                     BoundingSphere bs1 = mesh1.BoundingSphere;
-                    bs1.Center = obj1.Position;
-                    bs1.Radius *= obj1.Scale;
+                    bs1.Center = obj1.Position2;
+                    bs1.Radius *= 0.8f * obj1.Scale;
                    
                     BoundingSphere bs2 = mesh2.BoundingSphere;
-                    bs2.Center = obj2.Position;
-                    bs2.Radius *= obj2.Scale;
+                    bs2.Center = obj2.Position2;
+                    bs2.Radius *= 0.8f * obj2.Scale;
 
                     if (bs1.Intersects(bs2))
                     {
